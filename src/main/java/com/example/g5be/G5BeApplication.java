3eb8e10a1ffd6d2b329372f5839536549cad4f8a
Chanuka Dilshan <mchanuka72@gmail.com>
2024-12-13 07:20:56 +0530

    add Swagger.yaml file

---
+++ src/main/java/com/example/g5be/G5BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.g5be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G5BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G5BeApplication.class, args);
+	}
+
+}
