5d84c10ee86e132fd585fedaa3b62d85ccb10bd9
Shashimal  Madhuwantha <168973162+ShashimalMadhuwantha@users.noreply.github.com>
2024-12-22 09:37:04 +0530

    Create Admin Model

---
+++ src/main/java/com/example/g5be/model/Admin.java
@@ -0,0 +1,43 @@
+package com.example.g5be.model;
+
+public class Admin {
+    private String aid;
+    private String name;
+    private String username;
+    private String password;
+
+    public String getAid() {
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    // Getters and Setters
+}
