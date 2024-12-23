cb9280b6103504a0535fac15531965127cc827ce
Shashimal  Madhuwantha <168973162+ShashimalMadhuwantha@users.noreply.github.com>
2024-12-23 13:48:05 +0530

    update security config

--- src/main/java/com/example/g5be/config/SecurityConfig.java
+++ src/main/java/com/example/g5be/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.g5be.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,17 +19,34 @@ public class SecurityConfig {
                 .csrf().disable() // Disable CSRF for APIs
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        "/api/v1/admin/login", // Admin login
-                        "/api/v1/lecturer/login", // Lecturer login
-                        "/api/v1/student/login", // Student login
-                        "/api/v1/admin/lecturer/register", // Lecturer registration endpoint
-                        "/api/v1/admin/lecturer/**", // Lecturer update endpoint
-                        "/api/v1/admin/lecturer/remove/**", // Lecturer delete endpoint
+                        "/api/auth/admin/login", // Admin login
+                        "/api/auth/lecturer/login", // Lecturer login
+                        "/api/auth/student/login", // Student login
+                        "/api/lecturer/register", // Lecturer registration endpoint
+                        "/api/lecturer/update/**", // Lecturer update endpoint
+                        "/api/lecturer/delete/**", // Lecturer delete endpoint
                         "/api/session/details", // Session details
-                        "/api/v1/admin/batches", // Badge registration
-                        "/api/v1/admin/student/register", // Student registration
-                        "/api/v1/admin/student/**", // Student update endpoint
-                        "/api/v1/admin/student/remove/**"  // Student delete endpoint
+                        "/api/badge/register", // Badge registration
+                        "/api/badge/lecturer/badges",
+                        "/lecturer/events/create/workshop",
+                        "/lecturer/events/workshops",
+                        "/api/student/register", // Student registration
+                        "/api/student/update/**", // Student update endpoint
+                        "/api/student/delete/**", // Student delete endpoint
+                        "/api/events/create",          // Create event
+                        "/api/events/list",            // List all events
+                        "/api/events/{eid}/update-status", // Update event status
+                        "/api/events/{eid}",           // Delete event
+                        "/api/events/details/**",     // View event details by ID
+                        "lecturer/events/create/announcement",
+                        "lecturer/events/announcements",
+                        "/api/student/batch/**",
+                        "lecturer/events/create/interview",
+                        "lecturer/events/lecturer/interviews",
+                        "admins/state",
+                        "admins/events",
+                        "lecturer/feedback/send",
+                        "lecturer/assign/interviews"
                 ).permitAll() // Allow unauthenticated access to these endpoints
                 .anyRequest().authenticated() // All other endpoints require authentication
                 .and()
