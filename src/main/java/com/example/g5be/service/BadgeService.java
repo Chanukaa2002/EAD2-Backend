0af203aef3e4949f67d3ef1c87173e1c86c943dc
Shashimal  Madhuwantha <168973162+ShashimalMadhuwantha@users.noreply.github.com>
2024-12-23 13:37:33 +0530

    update function for relevent teachers badge/get function

--- src/main/java/com/example/g5be/service/BadgeService.java
+++ src/main/java/com/example/g5be/service/BadgeService.java
@@ -1,10 +1,11 @@
 package com.example.g5be.service;
 
-
 import com.example.g5be.model.Badge;
 import com.example.g5be.repository.BadgeRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BadgeService {
@@ -18,13 +19,18 @@ public class BadgeService {
     }
 
     public void registerBadge(Badge badge) {
-        // Check if the session role is admin
         String role = (String) httpSession.getAttribute("role");
         if (role == null || !role.equals("ROLE_ADMIN")) {
             throw new RuntimeException("Access Denied: Only admins can register badges.");
         }
-
-        // Save the badge
         badgeRepository.save(badge);
+    }
+
+    public List<Badge> getBadgesByLecturer(String lecturerId) {
+        String role = (String) httpSession.getAttribute("role");
+        if (role == null || (!role.equals("ROLE_LECTURER") && !role.equals("ROLE_ADMIN"))) {
+            throw new RuntimeException("Access Denied: Only Lecturers or admins can access badges.");
+        }
+        return badgeRepository.findBadgesByLecturer(lecturerId);
     }
 }
