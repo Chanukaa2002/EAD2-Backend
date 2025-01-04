e9890afbf87adccc8709a65019dc7384297101f8
Shashimal  Madhuwantha <168973162+ShashimalMadhuwantha@users.noreply.github.com>
2025-01-04 15:02:26 +0530

    fix end point

--- src/main/java/com/example/g5be/controller/AdminController.java
+++ src/main/java/com/example/g5be/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/admins")
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
     private final AdminService adminService;
